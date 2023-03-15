d1442f3e7928d78575662c3e098d2db1490e38de
kevin ikahu <kevinikahu4@gmail.com>
2023-03-15 05:35:24 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+// Define company name
+const companyName = 'Scuber';
+
+// Define most profitable neighborhood
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define company CEO
+let companyCeo = 'Susan Smith';
+
